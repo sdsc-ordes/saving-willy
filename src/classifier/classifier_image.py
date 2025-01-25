5a8836ef4b486c851f5d2a0505c2603fa9ce2db8
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2025-01-25 08:40:30 +0100

    feat: hash used as identifier

--- src/classifier/classifier_image.py
+++ src/classifier/classifier_image.py
@@ -12,21 +12,20 @@ from utils.grid_maker import gridder
 from utils.metadata_handler import metadata2md
 
 def cetacean_classify(cetacean_classifier):
-    files = st.session_state.files
     images = st.session_state.images
     observations = st.session_state.observations
-
-    batch_size, row_size, page = gridder(files)
+    hashes = st.session_state.image_hashes
+    batch_size, row_size, page = gridder(hashes)
     
     grid = st.columns(row_size)
     col = 0
-
-    for file in files: 
-        image = images[file.name]
+    o=1
+    for hash in hashes: 
+        image = images[hash]
         
         with grid[col]:
             st.image(image, use_column_width=True)
-            observation = observations[file.name].to_dict()
+            observation = observations[hash].to_dict()
             # run classifier model on `image`, and persistently store the output
             out = cetacean_classifier(image) # get top 3 matches
             st.session_state.whale_prediction1 = out['predictions'][0]
@@ -44,14 +43,14 @@ def cetacean_classify(cetacean_classifier):
                 # get index of pred1 from WHALE_CLASSES, none if not present
                 print(f"[D] pred1: {pred1}")
                 ix = viewer.WHALE_CLASSES.index(pred1) if pred1 in viewer.WHALE_CLASSES else None
-                selected_class = st.selectbox(f"Species for {file.name}", viewer.WHALE_CLASSES, index=ix)
+                selected_class = st.selectbox(f"Species for observation {str(o)}", viewer.WHALE_CLASSES, index=ix)
             
             observation['predicted_class'] = selected_class
             if selected_class != st.session_state.whale_prediction1:
                 observation['class_overriden'] = selected_class
             
             st.session_state.public_observation = observation
-            st.button(f"Upload observation for {file.name} to THE INTERNET!", on_click=push_observations)
+            st.button(f"Upload observation {str(o)} to THE INTERNET!", on_click=push_observations)
             # TODO: the metadata only fills properly if `validate` was clicked.
             st.markdown(metadata2md())
 
@@ -62,7 +61,8 @@ def cetacean_classify(cetacean_classifier):
 
             whale_classes = out['predictions'][:]
             # render images for the top 3 (that is what the model api returns)
-            st.markdown(f"Top 3 Predictions for {file.name}")
+            st.markdown(f"Top 3 Predictions for observation {str(o)}")
             for i in range(len(whale_classes)):
                 viewer.display_whale(whale_classes, i)
+        o += 1
         col = (col + 1) % row_size
--- src/utils/grid_maker.py
+++ src/utils/grid_maker.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import math
 
-def gridder(files):
+def gridder(items):
     cols = st.columns(3)
     with cols[0]:
         batch_size = st.select_slider("Batch size:",range(10,110,10), value=10)
     with cols[1]:
         row_size = st.select_slider("Row size:", range(1,6), value = 5)
-    num_batches = math.ceil(len(files)/batch_size)
+    num_batches = math.ceil(len(items)/batch_size)
     with cols[2]:
         page = st.selectbox("Page", range(1,num_batches+1))
     return batch_size, row_size, page
