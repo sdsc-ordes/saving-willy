a5ced3eafe6cb947c4389cb745a66c97a8833262
rmm <rob.mills@epfl.ch>
2025-02-01 10:08:27 +0100

    chore: cleanup variable names and add docstrings

--- src/utils/workflow_ui.py
+++ src/utils/workflow_ui.py
@@ -1,18 +1,41 @@
 import streamlit as st
 
-def refresh_progress():
+def refresh_progress_display() -> None:
+    """
+    Updates the workflow progress display in the Streamlit sidebar.
+    """
     with st.sidebar:
-        tot = st.session_state.workflow_fsm.num_states - 1
-        cur_i = st.session_state.workflow_fsm.current_state_index
-        cur_t = st.session_state.workflow_fsm.current_state
-        st.session_state.disp_progress[0].markdown(f"*Progress: {cur_i}/{tot}. Current: {cur_t}.*")
-        st.session_state.disp_progress[1].progress(cur_i/tot)
+        num_states = st.session_state.workflow_fsm.num_states - 1
+        current_state_index = st.session_state.workflow_fsm.current_state_index
+        current_state_name = st.session_state.workflow_fsm.current_state
+        colour = 'primary' 
+        if current_state_index == num_states: # highlight that we finished
+            colour = 'green'
+        status = f":{colour}[*Progress: {current_state_index}/{num_states}. Current: {current_state_name}.*]"
+        
+        st.session_state.disp_progress[0].markdown(status)
+        st.session_state.disp_progress[1].progress(current_state_index/num_states)
+
     
-def init_workflow_viz():
+def init_workflow_viz(debug:bool=True) -> None:
+    """
+    Set up the streamlit elements for visualising the workflow progress.
+
+    Adds placeholders for progress indicators, and adds a button to manually refresh
+    the displayed progress. Note: The button is mainly a development aid.
+
+    Args:
+        debug (bool): If True, include the manual refresh button. Default is True.
+        
+    """
+
+    
+    #Initialise the layout containers used in the input handling
     # add progress indicator to session_state
     if "progress" not in st.session_state:
         with st.sidebar:
             st.session_state.disp_progress = [st.empty(), st.empty()]
-            # add button to sidebar, with the callback to refesh_progress
-            st.sidebar.button("Refresh Progress", on_click=refresh_progress)
+            if debug:
+                # add button to sidebar, with the callback to refesh_progress
+                st.sidebar.button("Refresh Progress", on_click=refresh_progress_display)
 
