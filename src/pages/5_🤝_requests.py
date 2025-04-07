6f0e32c4a82a11c82e355ae19fcbf5ebe46c1074
Laure Vancau <laure.vancauwenberghe@epfl.ch>
2025-04-07 13:31:17 +0200

    feat: extract dataset manipulation from map features

--- src/pages/5_🤝_requests.py
+++ src/pages/5_🤝_requests.py
@@ -6,3 +6,12 @@ st.set_page_config(
 )
 
 from utils.st_logs import parse_log_buffer, init_logging_session_states
+
+from datasets import disable_caching
+disable_caching()
+
+############################################################
+# the dataset of observations (hf dataset in our space)
+dataset_id = "Saving-Willy/temp_dataset"
+data_files = "data/train-00000-of-00001.parquet"
+############################################################
